98ed3efa0242369520e386f1e819b67b71156b54
whs <hwong@seekerscapital.com>
2020-05-18 16:48:03 +0800

    Add regexp

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"regexp"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
@@ -19,10 +20,12 @@ func main() {
 		log.Fatal(err)
 	}
 
+	r, _ := regexp.Compile("^.*/(.*)")
+
 	refs, _ := r.References()
 	refs.ForEach(func(ref *plumbing.Reference) error {
 		if ref.Type() == plumbing.HashReference && ref.Hash() == headRef.Hash() && ref.Name() != "HEAD" {
-			fmt.Println(ref.Name())
+			fmt.Println(r.FindStringSubmatch(ref.Name())[1])
 		}
 
 		return nil
